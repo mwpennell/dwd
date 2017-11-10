d066592c2e5d1b4550d76fbf76f97ab2644607c4
Fiona Beaty <fionabeaty@dhcp-206-87-156-101.ubcsecure.wireless.ubc.ca>
2017-11-09 16:29:10 -0800

    This changed the file name to Woodiness

---
+++ Nov 9th changes.R
@@ -0,0 +1,4 @@
+#This is testing, adding some text? 
+woodines <- file.choose(read.csv("/Users/fionabeaty/Dropbox/Masters/M1.1/BIOL 538O/dwd/data/woodines_dat.csv"))
+#This changes the file's name
+Woodiness <- woodines
